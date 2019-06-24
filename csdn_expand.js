0306109f5a84fb899e432878d1d0e7eef0b1cbc6
hanqiuss <602600232@qq.com>
2019-06-24 14:08:32 +0800

    Update csdn_expand.js

--- csdn_expand.js
+++ csdn_expand.js
@@ -4,18 +4,31 @@
 // @icon         https://csdnimg.cn/public/favicon.ico
 // @updateURL    https://raw.githubusercontent.com/hanqiuss/tampermonkeyTools/master/csdn_expand.js
 // @downloadURL  https://raw.githubusercontent.com/hanqiuss/tampermonkeyTools/master/csdn_expand.js
-// @version      0.01
+// @version      0.02
 // @run-at       document-idle
 // @author       You
 // @match        https://blog.csdn.net/*
+// @match        https://bbs.csdn.net/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var a = document.getElementById('article_content')
-    if(a){a.style=''}
-    a = document.getElementsByClassName('hide-article-box')
-    a[0].remove()
+    var a;
+    switch(window.location.host){
+        case 'blog.csdn.net':
+            a = document.getElementById('article_content')
+            if(a){a.style=''}
+            a = document.getElementsByClassName('hide-article-box')
+            a[0].remove()
+            break
+        case 'bbs.csdn.net':
+            a = document.getElementById('bbs_detail_wrap')
+            if(a){a.style=''}
+            a = document.getElementsByClassName('hide_topic_box')
+            a[0].remove()
+            break
+    }
+    
     // Your code here...
 })();
