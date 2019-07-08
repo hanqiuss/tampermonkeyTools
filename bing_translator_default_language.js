9591581dfb87a3826f0c6f483c8c2c67a144638a
hanqiuss <602600232@qq.com>
2019-07-08 11:29:20 +0800

    Update bing_translator_default_language.js

--- bing_translator_default_language.js
+++ bing_translator_default_language.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name         必应翻译默认语言改为中文
 // @namespace    http://tampermonkey.net/
+// @updateURL    https://raw.githubusercontent.com/hanqiuss/tampermonkeyTools/master/bing_translator_default_language.js
+// @downloadURL  https://raw.githubusercontent.com/hanqiuss/tampermonkeyTools/master/bing_translator_default_language.js
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
@@ -13,8 +15,6 @@
     'use strict';
     var e = document.getElementById('tta_tgtsl');
     if(e){e.value="zh-Hans"}
-    e = document.getElementById('t_tl').value="zh-CHS";
+    e = document.getElementById('t_tl');
     if(e){e.value="zh-CHS"}
-
-    // Your code here...
 })();
