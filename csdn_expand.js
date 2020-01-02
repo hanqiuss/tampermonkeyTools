fdaebc1d8f4ae271b29d46f97a59077fb5393219
hanqiuss <602600232@qq.com>
2020-01-02 11:49:05 +0800

    Update csdn_expand.js

--- csdn_expand.js
+++ csdn_expand.js
@@ -4,10 +4,11 @@
 // @icon         https://csdnimg.cn/public/favicon.ico
 // @updateURL    https://raw.githubusercontent.com/hanqiuss/tampermonkeyTools/master/csdn_expand.js
 // @downloadURL  https://raw.githubusercontent.com/hanqiuss/tampermonkeyTools/master/csdn_expand.js
-// @version      0.03
+// @version      0.04
 // @run-at       document-idle
 // @author       You
 // @match        https://blog.csdn.net/*
+// @match        https://*.blog.csdn.net/*
 // @match        https://bbs.csdn.net/*
 // @grant        none
 // ==/UserScript==
@@ -16,7 +17,13 @@
     'use strict';
     var a;
     switch(window.location.host){
-        case 'blog.csdn.net':
+        case 'bbs.csdn.net':
+            a = document.getElementById('bbs_detail_wrap')
+            if(a){a.style=''}
+            a = document.getElementsByClassName('hide_topic_box')
+            a[0].remove()
+            break
+        default :
             a = document.getElementById('article_content')
             if(a){a.style=''}
             a = document.getElementsByClassName('hide-article-box')
@@ -26,12 +33,6 @@
             if(a){a[0].style=''}
             a = document.getElementById('btnMoreComment')
             if(a){a.innerHTML = '<span> </span>'}
-            break
-        case 'bbs.csdn.net':
-            a = document.getElementById('bbs_detail_wrap')
-            if(a){a.style=''}
-            a = document.getElementsByClassName('hide_topic_box')
-            a[0].remove()
             break
     }
     
