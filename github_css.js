77ce654b3cfe3e75b36700e4cd85565908c90af4
hanqiuss <602600232@qq.com>
2020-07-27 10:36:16 +0800

    fix bug

--- github_css.js
+++ github_css.js
@@ -1,23 +1,29 @@
 // ==UserScript==
 // @name         github css
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @updateURL    https://raw.githubusercontent.com/hanqiuss/tampermonkeyTools/master/github_css.js
 // @downloadURL  https://raw.githubusercontent.com/hanqiuss/tampermonkeyTools/master/github_css.js
 // @description  try to take over the world!
 // @author       You
+// @run-at       document-idle
 // @match        https://github.com/*/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-let list1 = document.getElementsByClassName('container-xl clearfix new-discussion-timeline')
-let list2 = document.getElementsByClassName('pagehead repohead readability-menu')
-if(list1.length && list2.length){
-  let src = list1[0]
-  let dst = list2[0]
-  let width = getComputedStyle(src)['max-width']
-  dst.style['max-width'] = width
-  dst.style['margin-inline']='auto'
-}
+    function run(){
+        let el = document.getElementsByTagName('main')
+        if(!el.length){
+            console.log('can\'t find main tag')
+            return
+        }
+        let main = el[0]
+        let src = main.children[1]
+        let dst = main.children[0]
+        let width = getComputedStyle(src)['max-width']
+        dst.style['max-width'] = width
+        dst.style['margin-inline']='auto'
+    }
+    setInterval(run,200);
 })();
