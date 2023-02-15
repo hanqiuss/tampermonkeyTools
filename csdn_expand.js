9c2aba8bc4c18006c9a0ec8807123fc599a54aea
hanqiuss <602600232@qq.com>
2023-02-16 02:33:58 +0800

    Update csdn_expand.js
    增加`去除代码复制限制`功能

--- csdn_expand.js
+++ csdn_expand.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         csdn自动展开
+// @name         csdn 知乎
+// @description  csdn自动展开、自动关闭登录页面、去除代码复制限制、知乎自动关闭登录页面
 // @namespace    https://github.com/hanqiuss/tampermonkeyTools/
 // @icon         https://csdnimg.cn/public/favicon.ico
 // @updateURL    https://raw.githubusercontent.com/hanqiuss/tampermonkeyTools/master/csdn_expand.js
 // @downloadURL  https://raw.githubusercontent.com/hanqiuss/tampermonkeyTools/master/csdn_expand.js
-// @version      0.10
+// @version      0.11
 // @run-at       document-idle
 // @author       You
 // @match        https://*.blog.csdn.net/*
@@ -14,6 +15,7 @@
 // @grant        none
 // ==/UserScript==
 
+var csdn_css_is_in = false;
 function block_zhihu(){
     let a = document.getElementsByClassName('Modal-wrapper')
     if(a.length){
@@ -23,12 +25,39 @@ function block_zhihu(){
 }
 function block_csdn(){
     if(document.getElementById('passportbox')){
-        document.getElementById('passportbox').style.display='';
-        document.getElementsByClassName('login-mark').item(0).style.display='';
+        document.getElementById('passportbox').remove();
+    }
+    if(document.getElementsByClassName('passport-login-container').length){
+        document.getElementsByClassName('passport-login-container').item(0).remove();
     }
     if(document.getElementById('writeGuide')){
-        document.getElementById('writeGuide').style.display='';
+        document.getElementById('writeGuide').remove();
     }
+
+    if(document.getElementsByClassName('hide-preCode-box').length){
+        document.getElementsByClassName('hide-preCode-box').item(0).parentElement.removeAttribute('class')
+        document.getElementsByClassName('hide-preCode-box').item(0).remove();
+    }
+    Array.from(document.getElementsByClassName('hljs-button')).map(x=>x.remove())
+    if(!csdn_css_is_in){
+        csdn_css();
+        csdn_css_is_in = true;
+    }
+}
+function csdn_css(){
+    let node = document.createElement ('style');
+    node.textContent = `
+    #content_views pre,
+    #content_views pre code{
+        -webkit-touch-callout: auto;
+        -webkit-user-select: auto;
+        -khtml-user-select: auto;
+        -moz-user-select: auto;
+        -ms-user-select: auto;
+        user-select: auto;
+    }
+    `;
+    document.getElementsByTagName('head')[0].appendChild(node);
 }
 (function() {
     'use strict';
@@ -47,7 +76,7 @@ function block_csdn(){
             if(a){a.style=''}
             a = document.getElementsByClassName('hide_topic_box')
             if(!a.length)break
-            a[0].remove()
+            a[0].remove();
             break
         default :
             console.log('blog');
@@ -64,7 +93,6 @@ function block_csdn(){
             a = document.getElementById('btnMoreComment')
             if(!a.length)break
             if(a){a.innerHTML = '<span> </span>'}
-
             break
     }
 
